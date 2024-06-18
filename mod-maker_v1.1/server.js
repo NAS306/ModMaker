604614598685c953d543fe1c5c8013c4154d81dd
NAS306 <369.kim.t.y@gmail.com>
2024-06-19 08:05:40 +0900

    added comment of how to upload to github

--- mod-maker_v1.1/server.js
+++ mod-maker_v1.1/server.js
@@ -1,5 +1,13 @@
 // server.js
 // 핫스팟은 서버 안켜짐
+// github 연동하는 법
+// git init
+// git remote add origin https://github.com/유저닉네임/리포지토리 이름.git (이미 리포지토리가 있을 경우)
+// git add . (모두 add) 또는 git add 파일이름.확장자 또는 디렉토리/파일이름.확장자 (예: git add mod-maker_v1.1/server.js)
+// git status (잘 올라갔는지 확인)
+// git commit -m "커밋 메시지" (여기까지는 로컬)
+// git remote -u origin 브랜치이름(예: main)
+
 const express = require('express');
 const path = require('path');
 
