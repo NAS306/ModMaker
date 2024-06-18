3c52b12b6810c2c8164a77033004e96821d3d0e1
NAS306 <114122902+NAS306@users.noreply.github.com>
2024-06-19 08:14:43 +0900

    Updated comment
    corrected the command comment

--- mod-maker_v1.1/server.js
+++ mod-maker_v1.1/server.js
@@ -6,7 +6,7 @@
 // git add . (모두 add) 또는 git add 파일이름.확장자 또는 디렉토리/파일이름.확장자 (예: git add mod-maker_v1.1/server.js)
 // git status (잘 올라갔는지 확인)
 // git commit -m "커밋 메시지" (여기까지는 로컬)
-// git remote -u origin 브랜치이름(예: main)
+// git push -u origin 브랜치이름(예: main)
 
 const express = require('express');
 const path = require('path');
